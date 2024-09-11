dd4b1e20be5dfcb8558352cfe5479d9a5f370c3f
Akila-PHP <akila-php@webappclouds.com>
2024-09-11 12:59:06 +0530

    Create AccountService.java

---
+++ back-end/bankingapp/src/main/java/com/banking/bankingapp/Service/AccountService.java
@@ -0,0 +1,7 @@
+package com.banking.bankingapp.Service;
+
+import com.banking.bankingapp.DTO.AccountDTO;
+
+public interface AccountService {
+    AccountDTO createAccount(AccountDTO accountDTO);
+}
