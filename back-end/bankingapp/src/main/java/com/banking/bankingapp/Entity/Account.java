a7aecaa4d72dcbdeeb4fa53d3e7d24b77c13c39a
Akila-PHP <akila-php@webappclouds.com>
2024-09-11 11:22:36 +0530

    Create Account.java

---
+++ back-end/bankingapp/src/main/java/com/banking/bankingapp/Entity/Account.java
@@ -0,0 +1,23 @@
+package com.banking.bankingapp.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_holder_name")
+    private String accounHolderName;
+    private double balance;
+
+}
